5c82bad9c5070367234a14f0f80f8e0ad6daaab0
DavidZahn <David-Zahn@hotmail.com>
2024-04-20 15:34:07 +0200

    L03

---
+++ A03_Einkaufsliste/liste.ts
@@ -0,0 +1,10 @@
+// namespace EventInspector {
+//     /*
+//     Aufgabe: <L02_EventInspector>
+//     // Name: <David Zahn>
+//     // Matrikel: <275010>
+//     // Datum: <.04.2024>
+//     // Quellen: <"">
+//     */
+
+//     // window.addEventListener("load", );
